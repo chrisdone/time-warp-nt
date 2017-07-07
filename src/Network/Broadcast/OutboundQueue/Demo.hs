de5e47951e9d9040ff891e09ff5d89d9104f4818
Edsko de Vries <edsko@well-typed.com>
2017-07-07 12:13:04 +0200

    Update policy. Bring demo back.

--- src/Network/Broadcast/OutboundQueue/Demo.hs
+++ src/Network/Broadcast/OutboundQueue/Demo.hs
@@ -1,8 +1,4 @@
-{-# LANGUAGE RecursiveDo #-}
-
 -- | Demo for the outbound queue
---
--- NOTE: Currently broken!
 module Network.Broadcast.OutboundQueue.Demo (relayDemo) where
 
 import Control.Concurrent
@@ -21,90 +17,57 @@ import qualified Network.Broadcast.OutboundQueue as OutQ
 -------------------------------------------------------------------------------}
 
 relayDemo :: IO ()
-relayDemo = mdo
-    let block :: String -> IO () -> IO ()
-        block label act = do
+relayDemo = do
+    let block :: String -> [Node] -> IO () -> IO ()
+        block label nodes act = do
           logMsg label
           act
---          flush relayer
+          mapM_ (OutQ.flush . nodeOutQ) nodes
           threadDelay 500000
 
     -- Set up some test nodes
 
-    nodeC1 <- newNode NodeCfg {
-                  nodeId          = "C"
-                , nodeType        = NodeCore
-                , nodeRecvDelay   = 0
---                , nodeMaxInFlight = 1
---                , nodeRelayDelay  = 500000
---                , nodePeers       = simplePeers [nodeR]
---                , nodeRelayer     = relayer
-                }
-    nodeE1 <- newNode NodeCfg {
-                  nodeId          = "E1"
-                , nodeType        = NodeEdge
-                , nodeRecvDelay   = 0
---                , nodeMaxInFlight = 1
---                , nodeRelayDelay  = 500000
---                , nodePeers       = simplePeers [nodeR]
---                , nodeRelayer     = relayer
-                }
-    nodeE2 <- newNode NodeCfg {
-                  nodeId          = "E2"
-                , nodeType        = NodeEdge
-                , nodeRecvDelay   = 0
---                , nodeMaxInFlight = 1
---                , nodeRelayDelay  = 500000
---                , nodePeers       = simplePeers [nodeR]
---                , nodeRelayer     = relayer
-                }
-    nodeR  <- newNode NodeCfg {
-                  nodeId          = "R1"
-                , nodeType        = NodeRelay
-                , nodeRecvDelay   = 0
---                , nodeMaxInFlight = 1
---                , nodeRelayDelay  = 0
---                , nodePeers       = simplePeers [nodeC1, nodeE1, nodeE2]
---                , nodeRelayer     = relayer
-                }
+    nodeC1 <- newNode $ NodeCfg "C"  NodeCore  (CommsDelay 0)
+    nodeE1 <- newNode $ NodeCfg "E1" NodeEdge  (CommsDelay 0)
+    nodeE2 <- newNode $ NodeCfg "E2" NodeEdge  (CommsDelay 0)
+    nodeR  <- newNode $ NodeCfg "R1" NodeRelay (CommsDelay 0)
 
-    block "* Basic relay test" $ do
+    setPeers nodeC1 [nodeR]
+    setPeers nodeE1 [nodeR]
+    setPeers nodeE2 [nodeR]
+    setPeers nodeR  [nodeC1, nodeE1, nodeE2]
+
+    block "* Basic relay test" [nodeE1, nodeC1, nodeR] $ do
       sendFrom nodeE1 (Msg MsgTransaction 0)
       sendFrom nodeC1 (Msg MsgBlockHeader 0)
 
-    block "* Rate limiting" $ do
+    block "* Rate limiting" [nodeE1, nodeR] $ do
+      -- Edge nodes are rate limited to 1 msg/sec to relay nodes
       forM_ [Msg MsgTransaction n | n <- [1..10]] $ \msg ->
         sendFrom nodeE1 msg
 
-    block "* Priorities" $ do
+    block "* Priorities" [nodeR] $ do
+      -- These transactions will _only_ be sent to the core nodes
       forM_ [Msg MsgTransaction n | n <- [11..20]] $ \msg ->
-        sendFrom nodeE1 msg
+        sendFrom nodeR msg
+      -- These blocks will be sent to both core nodes and relay nodes, but
+      -- the transactions sent to the core nodes will take precedence over
+      -- the block headers sent to the core nodes.
       forM_ [Msg MsgBlockHeader n | n <- [11..20]] $ \msg ->
-        sendFrom nodeC1 msg
+        sendFrom nodeR msg
 
     -- Two core nodes that communicate directly with each other
     -- (disjoint from the nodes we set up above)
 
-    nodeC2 <- newNode NodeCfg {
-                  nodeId          = "C"
-                , nodeType        = NodeCore
-                , nodeRecvDelay   = 0
---                , nodeMaxInFlight = 1
---                , nodeRelayDelay  = 0
---                , nodePeers       = simplePeers [nodeC3]
---                , nodeRelayer     = relayer
-                }
-    nodeC3 <- newNode NodeCfg {
-                  nodeId          = "C"
-                , nodeType        = NodeCore
-                , nodeRecvDelay   = 500000
---                , nodeMaxInFlight = 2
---                , nodeRelayDelay  = 0
---                , nodePeers       = simplePeers [nodeC2]
---                , nodeRelayer     = relayer
-                }
+    nodeC2 <- newNode $ NodeCfg "C2" NodeCore (CommsDelay 0)
+    nodeC3 <- newNode $ NodeCfg "C3" NodeCore (CommsDelay 500000)
 
-    block "* Latency masking" $ do
+    setPeers nodeC2 [nodeC3]
+
+    block "* Latency masking" [nodeC2] $ do
+      -- Core to core communication is allowed higher concurrency
+      -- Although that's not explicitly rate limited, we've set up the nodes to
+      -- model slow communication
       forM_ [Msg MsgBlockHeader n | n <- [21..30]] $ \msg ->
         sendFrom nodeC2 msg
 
@@ -130,12 +93,6 @@ newDemoQ selfType = do
     return demoQ
   where
     sendMsg = \msg Node{..} -> send nodeChan msg
-{-
-    policy  = RelayerPolicy {
-        rPolicyDelay       = return . Just . nodeRelayDelay . nodeCfg
-      , rPolicyMaxInFlight = nodeMaxInFlight . nodeCfg
-      }
--}
 
 {-------------------------------------------------------------------------------
   Model of a node
@@ -154,7 +111,7 @@ data NodeCfg = NodeCfg {
       -- | Delay on synchronous communication
       --
       -- Used to model slow nodes
-    , nodeRecvDelay :: Delay
+    , nodeCommsDelay :: CommsDelay
     }
 
 data Node = Node {
@@ -174,12 +131,15 @@ newNode nodeCfg@NodeCfg{..} = do
     nodeChan    <- newSyncVar
     nodeMsgPool <- newMsgPool
     _tid <- forkIO $ forever $ do
-      msg   <- recv nodeChan nodeRecvDelay
+      msg   <- recv nodeChan nodeCommsDelay
       added <- addToMsgPool nodeMsgPool msg
       when added $ do
         logMsg $ nodeId ++ ": received " ++ show msg
         OutQ.enqueue nodeOutQ msg OutQ.OriginForward mempty
     return Node{..}
+
+setPeers :: Node -> [Node] -> IO ()
+setPeers Node{..} = OutQ.subscribe nodeOutQ . OutQ.simplePeers
 
 sendFrom :: Node -> Msg -> IO ()
 sendFrom Node{nodeCfg = NodeCfg{..}, ..} msg = do
@@ -211,7 +171,7 @@ addToMsgPool pool msg = modifyMVar pool $ \msgs -> return $!
 data SyncVar a = SyncVar (MVar (a, MVar ()))
 
 -- | Delay models slow communication networks
-type Delay = Int
+newtype CommsDelay = CommsDelay Int
 
 newSyncVar :: IO (SyncVar a)
 newSyncVar = SyncVar <$> newEmptyMVar
@@ -222,8 +182,8 @@ send (SyncVar v) a = do
     putMVar v (a, ack)
     takeMVar ack
 
-recv :: SyncVar a -> Delay -> IO a
-recv (SyncVar v) delay = do
+recv :: SyncVar a -> CommsDelay -> IO a
+recv (SyncVar v) (CommsDelay delay) = do
     (a, ack) <- takeMVar v
     -- We run the acknowledgement in a separate thread, to model a node
     -- spawning a listener for each incoming request
